5cf265012a6c7f48871cb9315909017b96773791
ErwinEsparto <espartoerwin15@gmail.com>
2024-01-30 01:49:44 +0800

    Added Add, Update, Delete functionality for Inventory

--- html/Inventory.php
+++ html/Inventory.php
@@ -18,6 +18,22 @@
         $conn = mysqli_connect($DBHost, $DBUser, $DBPass, $DBName);
         $fetcbdata = "SELECT * FROM tbl_inventory;";
         $result = mysqli_query($conn, $fetcbdata);
+
+        if(isset($_POST["submit"])){
+            $newItem = $_POST["newMedicalSupplies"];
+            $newQuantity = $_POST["newQuantity"];
+    
+            $aquery = "INSERT INTO tbl_inventory values ('', '$newItem','$newQuantity')";
+            $addquery = mysqli_query ($conn, $aquery);
+            header("location:Inventory.php");
+            die();
+        }
+        else if(isset($_GET['fld_itemID'])){
+            $query = "DELETE FROM tbl_inventory WHERE fld_itemID = '$_GET[fld_itemID]'";
+            $delete = mysqli_query ($conn, $query);
+            header("location:Inventory.php");
+            die();
+        }
     ?>
 
     <div class="container">
@@ -51,23 +67,33 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach($result as $row): ?>
-                        <tr class="choice">
-                            <td><?= htmlspecialchars($row['fld_itemID']) ?></td>
-                            <td><?= htmlspecialchars($row['fld_item']) ?></td>
-                            <td><?= htmlspecialchars($row['fld_quantity']) ?></td>
-                            <td><a href='edit.php?id=1'>Edit</a> | <a href='delete.php?id=1'>Delete</a></td>
-                        </tr>
-                    <?php endforeach ?>
+                <?php
+                    $rownum = mysqli_num_rows($result);
+
+                    if($rownum>0){
+                        while ($items = mysqli_fetch_assoc($result)){
+                            echo "
+                            <tr class='choice'>
+                                <td>".$items['fld_itemID']."</td>
+                                <td>".$items['fld_item']."</td>
+                                <td>".$items['fld_quantity']."</td>
+                                <td>
+                                    <a href='EditInventory.php?fld_itemID=".$items['fld_itemID']."'> Edit </a> |
+                                    <a href='Inventory.php?fld_itemID=".$items['fld_itemID']."'> Delete </a>
+                                </td>
+                            </tr>";
+                        }
+                    }
+                    ?>
                 </tbody>
             </table>
 
-            <form action="add.php" method="post">
+            <form method="POST">
                 <label for="newMedicalSupplies">New Medical Supplies:</label>
                 <input type="text" name="newMedicalSupplies" required>
                 <label for="newQuantity">New Quantity:</label>
                 <input type="text" name="newQuantity" required>
-                <input type="submit" value="Add">
+                <input type="submit" name="submit" value="Add">
             </form>
         </section>
     </div>
